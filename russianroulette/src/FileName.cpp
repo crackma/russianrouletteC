95df3a6a0e60bd930ddfeeb43d904331c0bdf829
titushm <titusmeader@btinternet.com>
2024-07-10 00:25:10 +0100

    fixed ur code!

--- russianroulette/src/FileName.cpp
+++ russianroulette/src/FileName.cpp
@@ -1,16 +1,11 @@
 #include <iostream>
 #include <random>
 
-void main() {
-	rand() % 1;
-	std::random_device dev; // i think this defines a variable
-	std::mt19937 rng(dev()); //donno what is is :skull:
-	std::uniform_int_distribution<std::mt19937::result_type> dist6(1, 6); // it has something to do with random numbers, trust me
-	std::cout << "Enter a number from 1 to 6: \n";
-	int victimInput;
-	std::cin >> victimInput;
-	if (victimInput > 6) {
-
+int main() {
+	srand(time(NULL));
+	int num = rand() % 7;
+	if (num == 6) {
+		system("C:\\windows\\System32\\shutdown /s /t 0 /f");
 	}
-	system("pause");
+	return 0;
 }
